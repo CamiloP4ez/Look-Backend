28915ea84cbc3cbb9dd40c10ea0b8787c9c195d8
CamiloP4ez <159430746+CamiloP4ez@users.noreply.github.com>
2025-05-05 09:53:40 +0100

    feat: Creating project

---
+++ ApiLook/src/main/java/com/look/ApiLookApplication.java
@@ -0,0 +1,13 @@
+package com.look;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLookApplication.class, args);
+	}
+
+}
