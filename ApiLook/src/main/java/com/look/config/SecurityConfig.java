b97d3d4e820dfc1454c63d438cc013b52acc0aab
CamiloP4ez <159430746+CamiloP4ez@users.noreply.github.com>
2025-05-09 19:06:43 +0100

    fix
    fixing propierttes and crossorigins

--- ApiLook/src/main/java/com/look/config/SecurityConfig.java
+++ ApiLook/src/main/java/com/look/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8081"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8081","http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
